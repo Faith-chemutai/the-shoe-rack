17a1eb6df98e399b48ac934bbd7894e92e7b213c
Faith <faithchemutai2330@gmail.com>
2023-05-06 13:00:26 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
-import Login from './components/Login';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+// import Home from "./components/Home";
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+    <div>
+      <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Navbar />} />
+      {/* <Route path="/" element={<Home />} /> */}
+
+
+      </Routes>
+      </div>
+    </BrowserRouter>
+  
    
   );
 }
